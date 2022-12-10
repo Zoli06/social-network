86355608d0a03f114bb23af2f65bbb02afb27c92
Zoli06 <zolixvagyok@gmail.com>
2022-12-10 22:59:47 +0100

    Dockerized app

--- server/db/sql_connect.js
+++ server/db/sql_connect.js
@@ -11,6 +11,25 @@ const pool = mysql.createPool({
   namedPlaceholders: true,
 });
 
+// check connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.");
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  } else {
+    console.log("Connected to database");
+  }
+  if (connection) connection.release();
+  return;
+});
+
 module.exports = {
   query: async (query, args) => {
     const connection = await pool.promise().getConnection();
