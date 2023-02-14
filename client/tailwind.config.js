58da9e5914e063f6b35b9945336232db68df0c5b
Zoli06 <zolixvagyok@gmail.com>
2023-02-14 20:28:23 +0100

    Corrections in tailwind and daisyUI setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
+  content: ['node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.{js,jsx,ts,tsx}'],
   plugins: [require('daisyui')],
 }
