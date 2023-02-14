1b8a1edcb4f5f7f616494772a2fe67b6bc29d864
Zoli06 <zolixvagyok@gmail.com>
2023-02-14 20:28:39 +0100

    Login page finished

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,6 +1,8 @@
 import './Login.scss';
 import { gql, useMutation } from '@apollo/client';
 import React from 'react';
+// daisyui
+import { Input, Button } from 'react-daisyui';
 
 const LOGIN_MUTATION = gql`
   mutation Login($email: String!, $password: String!) {
@@ -16,6 +18,7 @@ export const Login = () => {
   );
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // TODO: trace value with state
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get('email');
@@ -39,13 +42,39 @@ export const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='email'>Email</label>
-      <input type='email' name='email' id='email' />
-      <label htmlFor='password'>Password</label>
-      <input type='password' name='password' id='password' />
-      <button type='submit'>Login</button>
-      <a href='/register'>Register</a>
+    // <form onSubmit={handleSubmit}>
+    //   <label htmlFor='email'>Email</label>
+    //   <input type='email' name='email' id='email' />
+    //   <label htmlFor='password'>Password</label>
+    //   <input type='password' name='password' id='password' />
+    //   <button type='submit'>Login</button>
+    //   <a href='/register'>Register</a>
+    // </form>
+
+    <form
+      className='flex component-preview p-4 items-center justify-center gap-2 font-sans'
+      onSubmit={handleSubmit}
+    >
+      <div className='form-control w-full max-w-xs flex flex-col gap-2'>
+        <div>
+          <h1 className='text-3xl font-bold'>Login</h1>
+          <label className='label'>
+            <span className='label-text'>Email address</span>
+          </label>
+          <Input type='email' name='email' id='email' />
+          <label className='label'>
+            <span className='label-text'>Password</span>
+          </label>
+          <Input type='password' name='password' id='password' />
+        </div>
+
+        <div>
+          <Button type='submit' className='w-full btn-primary'>
+            Login
+          </Button>
+          <a href='/register'>Register</a>
+        </div>
+      </div>
     </form>
   );
 };
