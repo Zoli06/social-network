e92f731f4545e67cb1aa2b981d6a29c2f5d42487
Zoli06 <zolixvagyok@gmail.com>
2023-03-15 17:42:13 +0100

    User points

--- client/src/components/User/UserDisplayedName.tsx
+++ client/src/components/User/UserDisplayedName.tsx
@@ -2,11 +2,25 @@ import { gql } from '@apollo/client';
 import React from 'react';
 
 export const UserDisplayedName = ({
-  user: { firstName, middleName, lastName },
+  user: { firstName, middleName, lastName, points },
 }: UserDisplayedNameProps) => {
+  const pointColor =
+    points > 0
+      ? 'text-green-500'
+      : points < 0
+      ? 'text-red-500'
+      : 'text-gray-500';
+
   return (
     <h1 className='text-xl md:text-2xl text-center'>
       {firstName} {middleName} {lastName}
+      <span className={`${pointColor} ml-2`}>
+        {(() => {
+          if (points > 0) return `+${points}`;
+          if (points < 0) return `-${points}`;
+          return `0`;
+        })()}
+      </span>
     </h1>
   );
 };
@@ -18,6 +32,7 @@ UserDisplayedName.fragments = {
       firstName
       lastName
       middleName
+      points
     }
   `,
 };
@@ -27,6 +42,7 @@ export type UserDisplayedNameGQLData = {
   firstName: string;
   lastName: string;
   middleName: string;
+  points: number;
 };
 
 type UserDisplayedNameProps = {
